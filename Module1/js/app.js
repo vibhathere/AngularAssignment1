89b3cc363bb45d3eb506e4aabbdf0ada7bf8f825
Vibha There <vibha@BITSDEV.IN>
2017-02-21 15:16:14 +0530

    added all files

---
+++ Module1/js/app.js
@@ -0,0 +1,9 @@
+(function(){
+    'use strict';
+    
+    angular.module("eating",[])
+            .controller("eatingController",function($scope){
+                $scope.list = "";
+                
+    });
+})();
